b1068127f911c3e5cfdfd3c7cbe1cc257e545102
张宏伟 <zhanghongwei-hd@139.com>
2023-02-28 11:04:50 +0800

    测试

--- small-spring-step-01/src/main/java/cn/bugstack/springframework/BeanDefinition.java
+++ small-spring-step-01/src/main/java/cn/bugstack/springframework/BeanDefinition.java
@@ -4,7 +4,7 @@ package cn.bugstack.springframework;
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public class BeanDefinition {
-
+    //测试修改第7行
     private Object bean;
 
     public BeanDefinition(Object bean) {
